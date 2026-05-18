149dbe32f97433811579908945ea21310a3db961
Relaced422 <Optquenum@gmail.com>
2026-05-18 16:04:05 +0200

    Made header.php and included in index.php

---
+++ html/header.php
@@ -0,0 +1,42 @@
+<header class="bg-[#A3C7A7] flex items-center justify-between px-6 py-4">
+    <a href="index.php">
+        <img src="img/logo.png" alt="HighFlights logo" class="w-[220px] max-w-full">
+    </a>
+
+    <nav>
+        <ul class="kadwa flex items-center gap-15 text-xl text-white">
+            <li>
+                <a href="index.php"
+                    class="block rounded-full bg-[#78B183] px-10 py-5 transition-all duration-300 hover:scale-105 hover:bg-[#497F53]">
+                    Home
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                    class="block rounded-full bg-[#78B183] px-10 py-5 transition-all duration-300 hover:scale-105 hover:bg-[#497F53]">
+                    Destinations
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                    class="block rounded-full bg-[#78B183] px-10 py-5 transition-all duration-300 hover:scale-105 hover:bg-[#497F53]">
+                    About Us
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                    class="block rounded-full bg-[#78B183] px-10 py-5 transition-all duration-300 hover:scale-105 hover:bg-[#497F53]">
+                    Contact
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <a href="__PLACEHOLDER__">
+        <img src="img/loginButton.png" alt="Login Button"
+            class="w-[220px] max-w-full transition-transform duration-300 hover:scale-105">
+    </a>
+</header>
