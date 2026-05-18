5a362fa85a094a27b4c7bb4c663d20f178b031c4
Fero <1208839@student.roc-nijmegen.nl>
2026-05-18 16:52:38 +0200

    Body background and first h1 box

--- html/index.php
+++ html/index.php
@@ -6,15 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HighFlights - High Quality Journeys</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kadwa:wght@400;700&display=swap');
+        body {
+            background-image: url('../img/background.png');
+        }
+    </style>
 </head>
 
-<body>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="bg-[#A3C7A7] p-6 text-center rounded-lg">
-        <h1>Your High (Quality) Journey Starts Here</h1>
-        <p>Your comfort is our top priority</p>
+<body class="kadwa bg-cover bg-center">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="bg-[#A3C7A7] p-6 text-center rounded-lg">
+            <div class="flex items-center">
+                <h1 class="text-2xl">Y</h1> <img src="img/weedIcon.png" class="w-5 h-5 inline-block align-middle"
+                    alt="">
+                <h1 class="text-2xl">ur High (Quality) J</h1><img src="img/weedIcon.png"
+                    class="w-5 h-5 inline-block align-middle" alt="">
+                <h1 class="text-2xl">urney</h1>
+                </h1>
+            </div>
+            <h1 class="text-2xl mt-3">Starts Here</h1>
+            <p class="mt-3">Your comfort is our top priority</p>
+        </div>
     </div>
-</div>
 </body>
 
 </html>
