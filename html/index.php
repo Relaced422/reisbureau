cf7f810c41f17ca626db1753639404841f79d208
Fero <1208839@student.roc-nijmegen.nl>
2026-05-18 16:09:50 +0200

    First push + Made a start on body for the index

--- html/index.php
+++ html/index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HighFlights - High Quality Journeys</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
-    
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="bg-[#A3C7A7] p-6 text-center rounded-lg">
+        <h1>Your High (Quality) Journey Starts Here</h1>
+        <p>Your comfort is our top priority</p>
+    </div>
+</div>
 </body>
+
 </html>
