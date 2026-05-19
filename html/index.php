7db591ad83b12331d2f8e3461534599688f11653
Relaced422 <Optquenum@gmail.com>
2026-05-19 11:19:49 +0200

    - Changed styling of Hero1 (Your High Quality....)
    - Added sections to index.php
    
    - Added php include

--- html/index.php
+++ html/index.php
@@ -18,22 +18,25 @@
 
 
 <body class="kadwa bg-cover bg-center">
-    <?php include 'header.php'; ?>
+    <?php include 'includes/header.php'; ?>
     <main>
-        <div class="flex items-center justify-center min-h-screen">
-            <div class="bg-[#A3C7A7] p-6 text-center rounded-lg">
-                <div class="flex items-center">
-                    <h1 class="text-2xl">Y</h1> <img src="img/icons/weedIcon.png" class="w-5 h-5 inline-block align-middle"
-                        alt="">
-                    <h1 class="text-2xl">ur High (Quality) J</h1><img src="img/icons/weedIcon.png"
-                        class="w-5 h-5 inline-block align-middle" alt="">
-                    <h1 class="text-2xl">urney</h1>
-                    </h1>
+        <section id="hero1">
+            <div class="flex items-center justify-center min-h-screen">
+                <div class="bg-[#A3C7A7] p-12 text-center rounded-3xl">
+                    <div class="flex items-center">
+                        <h1 class="text-2xl">Y</h1> <img src="img/icons/weedIcon.png"
+                            class="w-5 h-5 inline-block align-middle" alt="">
+                        <h1 class="text-2xl">ur High (Quality) J</h1><img src="img/icons/weedIcon.png"
+                            class="w-5 h-5 inline-block align-middle" alt="">
+                        <h1 class="text-2xl">urney</h1>
+                        </h1>
+                    </div>
+                    <h1 class="text-2xl mt-3">Starts Here</h1>
+                    <p class="mt-3">Your comfort is our top priority</p>
                 </div>
-                <h1 class="text-2xl mt-3">Starts Here</h1>
-                <p class="mt-3">Your comfort is our top priority</p>
             </div>
-        </div>
+        </section>
+        <?php include 'searchbar.php'; ?>
     </main>
 </body>
 
